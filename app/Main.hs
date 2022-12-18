5dfeb3fc683e7d06eb68426afb83e91d2ddb5d83
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2022-12-18 20:49:53 +0700

    add example function that take one argument and return one argument
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

--- app/Main.hs
+++ app/Main.hs
@@ -2,15 +2,28 @@
 
 module Main where
 
-import Foreign.C.Types
+-- import Haskell type representing the C int type
+import Foreign.C.Types ( CInt(..) )
 
--- Import the sayHello function from the function.o object file
+-- Import the sayHello & factorial function
+-- from the function.o object file
 foreign import ccall "sayHello" sayHello :: IO CInt
+foreign import ccall "factorial" factorial :: CInt -> IO CInt
+
+-- use factorial function from c Foreign Function
+doFactorial :: Int -> IO Int
+doFactorial n = do
+  result <- factorial (fromIntegral n)
+  return (fromIntegral result)
 
 -- Define the main function that calls the sayHello function
 main :: IO ()
 main = do
+  -- Bind doFactorial function to dofac
+  dofac <- doFactorial 5
+  putStrLn $  "factorial 5 is " ++ show dofac
   -- Bind the return value of the sayHello function to the _ variable
   _ <- sayHello
-  -- Return an empty tuple () to indicate that the main function has a return type of IO ()
+  -- Return an empty tuple () to indicate that 
+  -- the main function has a return type of IO ()
   return ()
--- cbits/function.c
+++ cbits/function.c
@@ -1,8 +1,23 @@
 // Include the standard input/output library
 #include <stdio.h>
 
-// Define a function called "sayHello" that takes no arguments and returns nothing
-void sayHello() {
-  // Print "hello world" to the console using the printf function
-  printf("hello from C program with Foreign Function Interface\n");
+// Define a function called "sayHello" that
+// takes no arguments and returns nothing
+void
+sayHello( )
+{
+  // Print "hello world" to the console 
+  // with printf function
+  printf("hello from C program\n");
+}
+
+// Define a function called factorial that
+// take one integer and return integer
+int
+factorial( int n )
+{
+  // if variable n equal 0 return 1
+  // if not it will call myself 
+  // that multiply n but with n - 1 as input
+  return (n == 0) ? 1 : n * factorial(n - 1);
 }
